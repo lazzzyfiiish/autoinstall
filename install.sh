533fbd7f4bc659ae56a48c186934545746b7a61d
lazzzyfiiish <lazzzyfiiish@gmail.com>
2025-12-05 02:57:32 +0800

    updated something

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
-# mihomo
-bash ./packages/mihomo
-bash ./configurations/mihomo
-bash ./services/mihomo
+#!/bin/bash
+echo "Desktop or Server"
+echo "(1) Desktop"
+echo "(2) Server"
+
+read -r -p "Number: " NUM
+
+case "$NUM" in
+	1)
+		bash ./enviroments/desktop
+	;;
+	2)
+		bash ./enviroments/server
+	;;
+esac
